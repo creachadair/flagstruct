0e56402b801116b5923b336213d1ac1e436921ee
M. J. Fromberger <michael.j.fromberger@gmail.com>
2015-11-26 13:26:06 -0800

    Add initial source for package flagstruct and its test.

---
+++ flagstruct.go
@@ -0,0 +1,122 @@
+// Package flagstruct supports automatic registration of struct fields as
+// flags.  Flags are associated with exported struct fields that have a tag
+// giving them a name and help text, for example:
+//
+//   flag:"flagname,help description"
+//
+package flagstruct
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+)
+
+// flagInfo captures the information needed to register a struct field in a
+// flag.FlagSet.
+type flagInfo struct {
+	field interface{} // must be of pointer type
+	name  string
+	help  string
+}
+
+// register registers fi with fs if fi.field implements flag.Value or is one of
+// the supported built-in types.
+func (fi *flagInfo) register(fs *flag.FlagSet) error {
+	switch t := fi.field.(type) {
+	case flag.Value:
+		fs.Var(t, fi.name, fi.help)
+	case *bool:
+		fs.BoolVar(t, fi.name, *t, fi.help)
+	case *time.Duration:
+		fs.DurationVar(t, fi.name, *t, fi.help)
+	case *float64:
+		fs.Float64Var(t, fi.name, *t, fi.help)
+	case *int64:
+		fs.Int64Var(t, fi.name, *t, fi.help)
+	case *int:
+		fs.IntVar(t, fi.name, *t, fi.help)
+	case *string:
+		fs.StringVar(t, fi.name, *t, fi.help)
+	case *uint64:
+		fs.Uint64Var(t, fi.name, *t, fi.help)
+	case *uint:
+		fs.UintVar(t, fi.name, *t, fi.help)
+	default:
+		return fmt.Errorf("type %T does not implement flag.Value", fi.field)
+	}
+	return nil
+}
+
+func (fi *flagInfo) String() string { return fmt.Sprintf("#<flag %q help=%q>", fi.name, fi.help) }
+
+// newFlagInfo extracts the flag name and help string from the tag of sf and
+// constructs a *flagInfo if possible.  If not, newFlagInfo returns nil, false.
+func newFlagInfo(sf reflect.StructField, v reflect.Value) (*flagInfo, bool) {
+	tag := sf.Tag.Get("flag")
+	if tag == "" || sf.PkgPath != "" {
+		return nil, false // no tag, or field is unexported
+	}
+	name := tag
+	help := tag
+	if i := strings.Index(tag, ","); i >= 0 {
+		name = tag[:i]
+		help = tag[i+1:]
+	}
+	return &flagInfo{
+		field: v.Addr().Interface(),
+		name:  name,
+		help:  help,
+	}, true
+}
+
+// parseFlags returns a flagInfo record for each field of v that supports
+// registration with 1the flag package.
+func parseFlags(v interface{}) ([]*flagInfo, error) {
+	s := reflect.ValueOf(v)
+	if s.Kind() != reflect.Ptr {
+		return nil, errors.New("value must be a pointer")
+	}
+	s = reflect.Indirect(s)
+	if s.Kind() != reflect.Struct {
+		return nil, errors.New("value must be a struct")
+	}
+
+	t := s.Type()
+	var flags []*flagInfo
+	for i := 0; i < s.NumField(); i++ {
+		fi, ok := newFlagInfo(t.Field(i), s.Field(i))
+		if ok {
+			flags = append(flags, fi)
+		}
+	}
+	return flags, nil
+}
+
+// Register adds flags to fs for each field of v that defines a flag.
+// It is an error if v is not a pointer to a struct value.
+//
+// An exported field is flaggable if it has a field tag of the form
+// `flag:"name,usage"` and a pointer to its type implements the flag.Value
+// interface.  As a special case, the built-in types supported by the flag
+// package are also allowed (bool, int, time.Duration, float64, etc.).
+//
+// Unexported fields, and fields without a flag tag are skipped without error;
+// however it is an error if there are no flaggable fields in the type.
+func Register(v interface{}, fs *flag.FlagSet) error {
+	flags, err := parseFlags(v)
+	if err != nil {
+		return err
+	} else if len(flags) == 0 {
+		return errors.New("struct contains no flaggable fields")
+	}
+	for _, fi := range flags {
+		if err := fi.register(fs); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ flagstruct_test.go
@@ -0,0 +1,125 @@
+package flagstruct
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+)
+
+func TestTypeErrors(t *testing.T) {
+	// Each of these types should generate an error from Register.
+	tests := []interface{}{
+		"foo",             // not a pointer, not a struct
+		37,                // ditto
+		new(int),          // not a struct
+		struct{ S int }{}, // not a pointer
+		&struct{}{},       // no flaggable fields
+		&struct { // no flaggable fields
+			z int     // unexported, no struct tag
+			r rune    `flag:"r,rune"` // unexported
+			F float64 // no struct tag
+			U uint    `json:"q"` // missing flag: clause in struct tag
+			S string  `flag:""`  // bogus flag: clause in struct tag
+		}{},
+		&struct { // no flaggable fields
+
+		}{},
+	}
+	fs := flag.NewFlagSet("dummy", flag.PanicOnError)
+	for _, bad := range tests {
+		if err := Register(bad, fs); err != nil {
+			t.Logf("Register(%T) gave expected error: %v", bad, err)
+		} else {
+			t.Errorf("Register(%T) expected error, but got %+v", bad, fs)
+		}
+	}
+}
+
+// dummy implements a no-op flag.Value for testing purposes.
+type dummy struct{}
+
+func (d *dummy) String() string     { return "" }
+func (d *dummy) Set(s string) error { return nil }
+
+func TestRegistration(t *testing.T) {
+	tests := []struct {
+		name, help string
+		input      interface{}
+	}{
+		{"dummy", "fake", &struct {
+			D dummy `flag:"dummy,fake"` // *D implements flag.Value directly
+		}{}},
+		{"b", "bool", &struct {
+			B bool `flag:"b,bool"`
+		}{}},
+		{"d", "duration", &struct {
+			D time.Duration `flag:"d,duration"`
+		}{}},
+		{"f64", "float64", &struct {
+			F float64 `flag:"f64,float64"`
+		}{}},
+		{"z64", "int64", &struct {
+			Z int64 `flag:"z64,int64"`
+		}{}},
+		{"z", "int", &struct {
+			Z int `flag:"z,int"`
+		}{}},
+		{"s", "string", &struct {
+			S string `flag:"s,string"`
+		}{}},
+		{"u64", "uint64", &struct {
+			UZ uint64 `flag:"u64,uint64"`
+		}{}},
+		{"u", "uint", &struct {
+			UZ uint `flag:"u,uint"`
+		}{}},
+		{"wat", "wat", &struct {
+			S string `flag:"wat"` // missing help is OK
+		}{}},
+	}
+	for _, test := range tests {
+		fs := flag.NewFlagSet("dummy", flag.PanicOnError)
+		if err := Register(test.input, fs); err != nil {
+			t.Errorf("Registering %T (%v) failed: %v", test.input, test.input, err)
+			continue
+		}
+		got := fs.Lookup(test.name)
+		if got == nil {
+			t.Errorf("Lookup %q failed: flag not found", test.name)
+			continue
+		}
+		if got.Usage != test.help {
+			t.Errorf("Flag %+v help: got %q, want %q", test.input, got.Usage, test.help)
+		}
+	}
+}
+
+func ExampleUsage() {
+	type Config struct {
+		Input  string `flag:"in,The path of the input file"`
+		Output string `flag:"out,The path of the output file"`
+		Count  int    `flag:"count,The number of lines to process"`
+		Other  string // field will not be flagged; there is no struct tag
+		inner  byte   // field will not be flagged; it is unexported
+	}
+
+	// Register a pointer to the config type with the flag set.  Default values
+	// are taken from the struct at registration time and non-flag fields are
+	// not touched.
+	c := &Config{Count: 17, Other: "p", inner: 'x'}
+	if err := Register(c, flag.CommandLine); err != nil {
+		log.Fatalf("Registration failed for %+v: %v", c, err)
+	}
+
+	// In a real program, you'd just call flag.Parse(); here we simulate that
+	// by invoking the parser directly on some arguments.
+	args := []string{"-in", "in.bin", "-out", "out.bin", "foo"}
+	if err := flag.CommandLine.Parse(args); err != nil {
+		log.Fatalf("flag.Parse() failed: %v", err)
+	}
+
+	fmt.Printf("in=%s out=%s count=%d other=%s inner=%c\n", c.Input, c.Output, c.Count, c.Other, c.inner)
+	// Output: in=in.bin out=out.bin count=17 other=p inner=x
+}
